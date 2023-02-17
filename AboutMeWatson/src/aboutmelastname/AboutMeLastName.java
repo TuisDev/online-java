ff1f78d53d2857fa1b5268e0537db339b097a775
TuisDev <99097079+TuisDev@users.noreply.github.com>
2023-02-17 14:01:57 -0500

    I AM GOD

---
+++ AboutMeWatson/src/aboutmelastname/AboutMeLastName.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aboutmelastname;
+
+/**
+ *
+ * @author jijwa
+ */
+public class AboutMeLastName {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("My name is Jacob Watson.");
+        System.out.println("I live in Goderich, Ontario.");
+        System.out.println("I love computer science, and I wanted to learn more about it.");
+        System.out.println("I hope to learn how to use the Java programing language.");
+        System.out.println("My favourite piece of technology is real time ray tracing.");
+        System.out.println("My favourite app of computer program is Visual Studio Code.");
+        System.out.println("I am vegan.");
+        
+    }
+    
+}
